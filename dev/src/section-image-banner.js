560d2a7068af67d16d608fb7af0823893e7e497c
SlavaFlava <parxomenko.90@inbox.ru>
2023-09-26 13:29:29 +0400

    added files for section

---
+++ dev/src/section-image-banner.js
@@ -0,0 +1,12 @@
+if (!customElements.get('image-banner')) {
+  customElements.define('image-banner', class ImageBanner extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+
+    }
+  }
+  )
+}
