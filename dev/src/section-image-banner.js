f2365848db6c3f1fc876322ec222279fd3338b63
SlavaFlava <parxomenko.90@inbox.ru>
2023-09-27 12:18:06 +0400

    added styles (finished section)

--- dev/src/section-image-banner.js
+++ dev/src/section-image-banner.js
@@ -6,12 +6,15 @@ if (!customElements.get('image-banner')) {
 
     connectedCallback() {
       this.speed = (this.getAttribute('data-speed'))
+      this.dataDots = (this.getAttribute('data-dots') == 'true' ? true : false)
       this.settings = {
         cellAlign: 'center',
         contain: false,
         wrapAround: true,
+        pageDots: this.dataDots,
         pageDots: true,
-        prevNextButtons: false
+        prevNextButtons: false,
+        percentPosition: false
       }
       this.speed == 'false' ? false : this.settings.autoPlay = +this.speed;
 
