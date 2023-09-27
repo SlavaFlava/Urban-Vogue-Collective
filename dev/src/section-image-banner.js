5d610e97ca9ad5b685675b2203a4c2df9e77a005
SlavaFlava <parxomenko.90@inbox.ru>
2023-09-27 10:17:43 +0400

    added slider and layout

--- dev/src/section-image-banner.js
+++ dev/src/section-image-banner.js
@@ -5,8 +5,23 @@ if (!customElements.get('image-banner')) {
     }
 
     connectedCallback() {
+      this.speed = (this.getAttribute('data-speed'))
+      this.settings = {
+        cellAlign: 'center',
+        contain: false,
+        wrapAround: true,
+        pageDots: true,
+        prevNextButtons: false
+      }
+      this.speed == 'false' ? false : this.settings.autoPlay = +this.speed;
 
+      this.initSlider();
     }
-  }
-  )
+
+    initSlider() {
+      this.slider = new Flickity(this.querySelector('.s-image-banner'), this.settings);
+    }
+
+  })
 }
+
