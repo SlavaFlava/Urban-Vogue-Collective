f7dc1fecaa7e555cdc995b9c62447f77e91a34e6
SlavaFlava <parxomenko.90@inbox.ru>
2023-10-06 19:17:04 +0400

    added mobile menu

--- dev/src/header.js
+++ dev/src/header.js
@@ -6,16 +6,31 @@ if (!customElements.get('s-header')) {
       }
 
       connectedCallback() {
-        this.mobMenu()
+        this.mobMenu() 
         this.megaMenu()
       }
 
       mobMenu() {
-
+        this.querySelector('.s-header__menu_btn').
+        addEventListener(
+          "click", () => (
+            this.querySelector('.s-header').classList.toggle('s-header--menu-active'),
+              document.querySelector('body').classList.toggle('hidden')
+          )
+        )
       }
 
       megaMenu() {
         const megaM = Array.from(this.querySelectorAll('.s-mega-menu'))
+        this.querySelectorAll('.s-mega-menu__close').forEach((closeMegaMenu) => {
+          closeMegaMenu.addEventListener('click', (e)=> {
+            this.querySelector('.s-header__menu').classList.remove('hide')
+              megaM.map((e) => {
+                e.classList.remove('active')
+              })
+            }
+          )
+        })
         this.querySelectorAll('.s-header__menu_item').forEach((menuItem) => {
 
           megaM.map((megaItem) => {
@@ -29,6 +44,14 @@ if (!customElements.get('s-header')) {
           menuItem.addEventListener('mouseout', (e) => {
             megaM.map((megaItem) => {
               e.target.innerHTML == megaItem.getAttribute('data-parent-menu') ? megaItem.classList.remove('active') : false
+            })
+          })
+          menuItem.querySelector('.s-header__menu_item_arrow').addEventListener('click', (e) => {
+            megaM.map((megaItem) => {
+              if (e.target.getAttribute('data-menu-name') == megaItem.getAttribute('data-parent-menu')){
+                megaItem.classList.add('active')
+                this.querySelector('.s-header__menu').classList.add('hide')
+              }
             })
           })
         })
