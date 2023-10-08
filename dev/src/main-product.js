2bcb7c61a63a2879bfeb9d26bcfec38a6460b8a3
SlavaFlava <parxomenko.90@inbox.ru>
2023-10-08 14:34:22 +0400

    added variants in js for main product

--- dev/src/main-product.js
+++ dev/src/main-product.js
@@ -4,9 +4,39 @@ customElements.define('main-product', class MProduct extends HTMLElement {
   }
 
   connectedCallback() {
+     this.variants = JSON.parse(this.getAttribute('data-variants')) 
+
+
+
+    this.options = this.querySelectorAll('.variants-option')
+
+    this.options.forEach(option => {
+      option.addEventListener('change', (e) => {
+        this.variantOnClick()
+      })
+    })
+
+
+
+  }
+
+  variantOnClick() {
+    let tempVariant = ''
+    this.options.forEach(variant => {
+      tempVariant += `${variant.querySelector("input:checked").value} / `
+     }
+    )
+    tempVariant = tempVariant.slice(0, -3)
+    console.log(tempVariant)
+    let currentVariantId = ''
+    this.variants.map((variant) => (
+     variant.title == tempVariant ? currentVariantId = variant.id : false
+    )) 
+
+    this.querySelector('#variants-select').value = currentVariantId
+   
   }
 
 }
 )
 
-  
